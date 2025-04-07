c641cb2c93e494c46bccf4a4f991e5223f1329e1
Spencer Davis <spencerdavis226@gmail.com>
2025-04-07 14:01:05 -0400

    Set up basic GraphQL API

--- app.js
+++ app.js
@@ -1,12 +1,33 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { graphqlHTTP } = require('express-graphql');
+const { buildSchema } = require('graphql');
 
 const app = express();
 
 app.use(bodyParser.json());
 
-app.get('/', (req, res, next) => {
-  res.send('Hello World!');
-});
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema: buildSchema(`
+      type RootQuery {
+        events: [String!]!
+      }
+      type RootMutation {
+        createEvent(name: String): String
+      }
+      schema {
+        query: RootQuery
+        mutation: RootMutation
+      }
+    `),
+    rootValue: {
+      events: () => ['Romantic Cooking', 'Sailing', 'All-Night Coding'],
+      createEvent: (args) => args.name,
+    },
+    graphiql: true,
+  })
+);
 
 app.listen(3000);
